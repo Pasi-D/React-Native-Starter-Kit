24a2ae5088bd6f0b88e90b8616b1ef97056f5aba
Pasindu Dilshan <Pasindu_dilshan@outlook.com>
2021-05-22 22:41:45 +0530

    Fix variable values in script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Make a copy of .env.dist
-echo "Updating version name"
+echo "Updating version name in .env"
 echo " - Version Name : $MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION-$PRE_RELEASE"
 
 cd ${APPCENTER_SOURCE_DIRECTORY}
@@ -9,7 +9,12 @@ cd ${APPCENTER_SOURCE_DIRECTORY}
 cp .env.dist .env
 
 # Replace environment variables
-sed -i '' 's/MAJOR_VERSION=.*/MAJOR_VERSION=$MAJOR_VERSION/' .env
-sed -i '' 's/MINOR_VERSION=.*/MINOR_VERSION=$MINOR_VERSION/' .env
-sed -i '' 's/PATCH_VERSION=.*/PATCH_VERSION=$PATCH_VERSION/' .env
-sed -i '' 's/PRE_RELEASE=.*/PRE_RELEASE=$PRE_RELEASE/' .env
+sed -i '' 's/MAJOR_VERSION=.*/MAJOR_VERSION=${MAJOR_VERSION}/' .env
+sed -i '' 's/MINOR_VERSION=.*/MINOR_VERSION=${MINOR_VERSION}/' .env
+sed -i '' 's/PATCH_VERSION=.*/PATCH_VERSION=${PATCH_VERSION}/' .env
+sed -i '' 's/PRE_RELEASE=.*/PRE_RELEASE=${PRE_RELEASE}/' .env
+
+# Print out .env for reference
+cat .env
+
+echo "updated .env!"
