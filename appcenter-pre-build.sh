dd6c6e547c3f117fbb1cde6fb4e7099ee4143d7a
Pasindu Dilshan <Pasindu_dilshan@outlook.com>
2021-05-22 22:12:49 +0530

    Fix script fails

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,8 +3,11 @@
 # Make a copy of .env.dist
 cp .env.dist .env
 
+echo "Updating version name"
+echo " - Version Name : $MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION-$PRE_RELEASE"
+
 # Replace environment variables
-sed -i 's/MAJOR_VERSION=.*/MAJOR_VERSION=${MAJOR_VERSION}/' .env
-sed -i 's/MINOR_VERSION=.*/MINOR_VERSION=${MINOR_VERSION}/' .env
-sed -i 's/PATCH_VERSION=.*/PATCH_VERSION=${PATCH_VERSION}/' .env
-sed -i 's/PRE_RELEASE=.*/PRE_RELEASE=${PRE_RELEASE}/' .env
+sed -i 's/MAJOR_VERSION=.*/MAJOR_VERSION=$MAJOR_VERSION/' .env
+sed -i 's/MINOR_VERSION=.*/MINOR_VERSION=$MINOR_VERSION/' .env
+sed -i 's/PATCH_VERSION=.*/PATCH_VERSION=$PATCH_VERSION/' .env
+sed -i 's/PRE_RELEASE=.*/PRE_RELEASE=$PRE_RELEASE/' .env
